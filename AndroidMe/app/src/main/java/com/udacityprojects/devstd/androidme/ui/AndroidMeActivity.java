3fc3df59d54e8dd41219fb801a7fd77518afcb68
Mehmet Durmaz <mehmetdurmazz@yahoo.com>
2018-10-07 01:05:55 +0300

    Starting code has been created.

---
+++ AndroidMe/app/src/main/java/com/udacityprojects/devstd/androidme/ui/AndroidMeActivity.java
@@ -0,0 +1,20 @@
+package com.udacityprojects.devstd.androidme.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.udacityprojects.devstd.androidme.R;
+
+
+// This activity will display a custom Android image composed of three body parts: head, body, and legs
+public class AndroidMeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_me);
+    }
+}
