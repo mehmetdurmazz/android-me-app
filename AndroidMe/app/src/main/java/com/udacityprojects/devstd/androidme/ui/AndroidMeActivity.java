3bfaa564d21ef6fdbe8747af413f4f355acf7859
Mehmet Durmaz <mehmetdurmazz@yahoo.com>
2018-10-10 21:58:16 +0300

    Other fragments added and working of multiple fragments tried in same activity.

--- AndroidMe/app/src/main/java/com/udacityprojects/devstd/androidme/ui/AndroidMeActivity.java
+++ AndroidMe/app/src/main/java/com/udacityprojects/devstd/androidme/ui/AndroidMeActivity.java
@@ -4,10 +4,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import com.udacityprojects.devstd.androidme.R;
+import com.udacityprojects.devstd.androidme.data.AndroidImageAssets;
 
 
 // This activity will display a custom Android image composed of three body parts: head, body, and legs
@@ -21,12 +19,33 @@ public class AndroidMeActivity extends AppCompatActivity {
         // BodyPartFragment instance
         BodyPartFragment headFragment = new BodyPartFragment();
 
+        // Set the list of image id's for the head fragment and set the position to the second image in the list
+        headFragment.setImageIdList(AndroidImageAssets.getHeads());
+        headFragment.setImageIdIndex(1);
+
+
         // Use a FragmentManager and transaction to add the fragment to the screen
         FragmentManager fragmentManager = getSupportFragmentManager();
 
         // Fragment transaction
         fragmentManager.beginTransaction()
-                .add(R.id.head_container,headFragment)
+                .add(R.id.head_container, headFragment)
                 .commit();
+
+
+        // Create and display the body and leg BodyPartFragments
+
+        BodyPartFragment bodyFragment = new BodyPartFragment();
+        bodyFragment.setImageIdList(AndroidImageAssets.getBodies());
+        fragmentManager.beginTransaction()
+                .add(R.id.body_container, bodyFragment)
+                .commit();
+
+        BodyPartFragment legFragment = new BodyPartFragment();
+        legFragment.setImageIdList(AndroidImageAssets.getLegs());
+        fragmentManager.beginTransaction()
+                .add(R.id.leg_container, legFragment)
+                .commit();
+
     }
 }
