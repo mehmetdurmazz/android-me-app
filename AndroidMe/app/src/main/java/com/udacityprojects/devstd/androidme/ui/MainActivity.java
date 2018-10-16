40c64b9a9718d51b369321400896461d3bfbb5a8
Mehmet Durmaz <mehmetdurmazz@yahoo.com>
2018-10-16 22:23:18 +0300

    Created a static fragment and added to an activity using XML.

---
+++ AndroidMe/app/src/main/java/com/udacityprojects/devstd/androidme/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.udacityprojects.devstd.androidme.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.udacityprojects.devstd.androidme.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
